34808227bd3f13f0b1cca67e144b002fbf87bb81
Fladit <zatsarin77@gmail.com>
2021-08-11 13:33:49 +0300

    replace manual redux actions in components to action-creators

--- src/action-creators/todo.ts
+++ src/action-creators/todo.ts
@@ -4,13 +4,23 @@ import {Dispatch} from "redux";
 
 function fetchTodos(limit: number = 10) {
     return async function(dispatch: Dispatch<TodoAction>) {
-        console.log("test")
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
-        const todos = response.data.map((todo:any): Todo => {
-            return {id: todo.id, isChecked: todo.completed, title: todo.title}
-        })
-        dispatch({type: TodosTypeEnum.LOAD, payload: todos})
+        try {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
+            const todos = response.data.map((todo:any): Todo => {
+                return {id: todo.id, isChecked: todo.completed, title: todo.title}
+            })
+            dispatch({type: TodosTypeEnum.LOAD, payload: todos})
+        } catch (e) {
+        }
     }
+}
+
+export function removeTodo(id: Todo['id']): TodoAction {
+    return {type: TodosTypeEnum.REMOVE, payload: id}
+}
+
+export function toggleDoneTodo(id: Todo['id']): TodoAction {
+    return {type: TodosTypeEnum.TOGGLE_DONE, payload: id}
 }
 
 export {fetchTodos}
--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -2,24 +2,18 @@ import React from 'react';
 import {Todo, TodosTypeEnum} from "../../types/todo";
 import './TodoItem.css'
 import useAppDispatch from "../../hooks/useAppDispatch";
+import {removeTodo, toggleDoneTodo} from "../../action-creators/todo";
 
 const TodoItem = ({todo}: TodoItemProps) => {
     const dispatch = useAppDispatch()
-    function handleCheckboxClick() {
-        dispatch({type: TodosTypeEnum.TOGGLE_DONE, payload: todo.id})
-    }
-
-    function removeTodo() {
-        dispatch({type: TodosTypeEnum.REMOVE, payload: todo.id})
-    }
     return (
         <div className={"todo"}>
             <div className={"todo-content-row"}>
                 <div className={"todo-content-row-title"}>
-                    <input type='checkbox' checked={todo.isChecked} onChange={handleCheckboxClick}/>
+                    <input type='checkbox' checked={todo.isChecked} onChange={() => dispatch(toggleDoneTodo(todo.id))}/>
                     <div className={todo.isChecked? 'todo-content-row__line-through' : ''}>{todo.title}</div>
                 </div>
-                <button onClick={removeTodo}/>
+                <button onClick={() => dispatch(removeTodo(todo.id))}/>
             </div>
             <div className={"todo-content-row"}>
                 <div>{todo.details}</div>
